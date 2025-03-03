bc4781ffdeab6046ffeaea1fa01ca3fc40e6e951
Aleksandr-stu-QA <solo.stu@mail.ru>
2025-03-03 15:02:15 +0300

    Update test_pokemons.py

--- PythonProjects/tests/test_pokemons.py
+++ PythonProjects/tests/test_pokemons.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 
 URL = 'https://api.pokemonbattle.ru/v2'
-TOKEN = 'fb1762798410d79339ef15f13f9790ec'
+TOKEN = 'token trainer'
 HEADER = {'Content-Type':'application/json','trainer_token':TOKEN}
 TRAINER_ID =22732
 
